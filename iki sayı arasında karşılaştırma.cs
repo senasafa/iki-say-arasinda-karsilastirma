810735f26f7ba4bdc7723e3a3a086790ab4b4bf9
senasafa <senasafa8@gmail.com>
2024-11-22 00:09:35 +0300

    Add files via upload

---
+++ iki sayı arasında karşılaştırma.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine("sayia");
+int sayia = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("sayib");
+int sayib = Convert.ToInt32(Console.ReadLine());
+
+if (sayia > sayib) 
+  {
+    Console.WriteLine(" sayia büyüktür");
+    }
+
+      else if (sayia < sayib) 
+{
+    Console.WriteLine(" sayib büyüktür");
+}
+
+
+  else
+    {
+       Console.WriteLine(" esittir");
+
+
+
+      }
+Console.ReadKey();
+
+
